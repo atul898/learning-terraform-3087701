4dee246dc2b2c42c3790d103717e5e90cbb0d79c
atul898 <achauhan@qumulo.com>
2023-09-10 23:06:01 -0600

    trying to get it going from git reference

--- main.tf
+++ main.tf
@@ -23,8 +23,6 @@ module "vpc" {
   azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
-
   tags = {
     Terraform = "true"
     Environment = "dev"
@@ -34,7 +32,8 @@ module "vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
+  
+  subnet_id                 = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids    = [module.blog_sg.security_group_id]
 
   tags = {
